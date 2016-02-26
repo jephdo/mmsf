83c1ecde982a8d3d5a144a8bfcfafa903d71529f
Jeph Do <jephdo@gmail.com>
2016-02-25 21:57:52 -0800

    test pulling from db

--- mmsf/main/views.py
+++ mmsf/main/views.py
@@ -2,7 +2,14 @@ from flask import render_template
 
 from . import main
 from .. import db
+from ..models import Kids
 
 @main.route('/hello')
 def hello():
     return 'hello world'
+
+
+@main.route('/kids/directory')
+def kids_directory():
+    kids = ['|'.join([kid.last_name, kid.first_name])for kid in Kids.query.all()]
+    return str(len(kids))
