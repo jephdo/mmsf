307ffb8e00ca803a5112e36c5872961ba8a8ea7a
Jeph Do <jephdo@gmail.com>
2016-02-26 20:44:29 -0800

    Add flask-bootstrap for html templating

--- mmsf/__init__.py
+++ mmsf/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.bootstrap import Bootstrap
 
 from config import config
 
-
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -11,8 +12,8 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    bootstrap.init_app(app)
     db.init_app(app)
-
 
     if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
         from flask.ext.sslify import SSLify
